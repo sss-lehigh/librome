d13e8106bbb17afd929674f48a25b2d252838be1
dePaul Miller <depaulmillz@users.noreply.github.com>
2023-09-12 13:08:06 -0400

    Add cuda build

---
+++ cicd/install_cuda_ubuntu.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
+sudo apt-get update
+sudo apt-get -y install cuda-compiler cuda-cudart-dev cuda-nvtx cuda-nvrtc-dev libcurand-dev cuda-cccl
+
+CUDA_PATH=/usr/local/cuda
+echo "CUDA_PATH=${CUDA_PATH}"
+export CUDA_PATH=${CUDA_PATH}
+export PATH="$CUDA_PATH/bin:$PATH"
+export LD_LIBRARY_PATH="$CUDA_PATH/lib:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$CUDA_PATH/lib64:$LD_LIBRARY_PATH"
+# Check nvcc is now available.
+nvcc -V
+
+echo "CUDA_PATH=$CUDA_PATH" >> "$GITHUB_ENV"
+echo "$CUDA_PATH/bin" >> "$GITHUB_ENV" # add to path
+echo "LD_LIBRARY_PATH=${LD_LIBRARY_PATH}" >> "$GITHUB_ENV"
+
